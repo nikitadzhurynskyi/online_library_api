bbab44283c4058212b0809dd039bb97a5c9265a4
nikitadzhurynskyi <nikitadzhurynskyi@gmail.com>
2026-01-26 01:21:44 +0000

    feat: base user and user role models created

---
+++ src/user/model.py
@@ -0,0 +1,20 @@
+import enum
+
+from sqlalchemy import Integer, String, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.db.database import Base
+
+
+class UserRole(str, enum.Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[String] = mapped_column(String, unique=True)
+    password: Mapped[String] = mapped_column(String)
+
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.USER)
